90af317ec5a69208ee0fac1dc5bfbf842ba07276
Kari Ferenczy <karbear2801@gmail.com>
2019-01-21 23:55:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
+function theBeatlesPlay(musicians, instruments){
+  var Beatles = [];
+  for (var i = 0; i < musicians.length; i++) {
+    Beatles.push(musicians[i] + "plays" + instruments[i]);
+  }
+  return Beatles;
+}
